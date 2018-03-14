a8c26e3ac6dfddf0807f5d2f9854bb300c51f8f5
Stefan Copetchi <kopezaur@gmail.com>
2018-03-14 01:50:45 +0100

    first commit

---
+++ OsProject/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OsProject
+{
+    partial class Form1
+    {
+        
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.navigateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.forwardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.goButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.homeButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.printScreenButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBoxOptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.navigateToolStripMenuItem,
+            this.pictureBoxOptionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1125, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // navigateToolStripMenuItem
+            // 
+            this.navigateToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.backToolStripMenuItem,
+            this.forwardToolStripMenuItem});
+            this.navigateToolStripMenuItem.Name = "navigateToolStripMenuItem";
+            this.navigateToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.navigateToolStripMenuItem.Text = "Navigate";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.homeToolStripMenuItem.Text = "Home";
+            this.homeToolStripMenuItem.Click += new System.EventHandler(this.homeToolStripMenuItem_Click);
+            // 
+            // backToolStripMenuItem
+            // 
+            this.backToolStripMenuItem.Name = "backToolStripMenuItem";
+            this.backToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.backToolStripMenuItem.Text = "Back <-";
+            this.backToolStripMenuItem.Click += new System.EventHandler(this.backToolStripMenuItem_Click);
+            // 
+            // forwardToolStripMenuItem
+            // 
+            this.forwardToolStripMenuItem.Name = "forwardToolStripMenuItem";
+            this.forwardToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.forwardToolStripMenuItem.Text = "Forward ->";
+            this.forwardToolStripMenuItem.Click += new System.EventHandler(this.forwardToolStripMenuItem_Click);
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(780, 27);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "http://google.ro",
+            "http://youtube.com",
+            "http://facebook.com",
+            "http://flashscore.com",
+            "http://mail.yahoo.com",
+            "http://9gag.com"});
+            this.comboBox1.Location = new System.Drawing.Point(94, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(680, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.MouseHover += new System.EventHandler(this.comboBox1_MouseHover);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 54);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1101, 599);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // homeButton
+            // 
+            this.homeButton.Location = new System.Drawing.Point(12, 27);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(75, 23);
+            this.homeButton.TabIndex = 4;
+            this.homeButton.Text = "Home";
+            this.homeButton.UseVisualStyleBackColor = true;
+            this.homeButton.Click += new System.EventHandler(this.homeButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 643);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1125, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Click += new System.EventHandler(this.toolStripProgressBar1_Click);
+            // 
+            // printScreenButton
+            // 
+            this.printScreenButton.Location = new System.Drawing.Point(862, 28);
+            this.printScreenButton.Name = "printScreenButton";
+            this.printScreenButton.Size = new System.Drawing.Size(75, 23);
+            this.printScreenButton.TabIndex = 6;
+            this.printScreenButton.Text = "PrintScreen";
+            this.printScreenButton.UseVisualStyleBackColor = true;
+            this.printScreenButton.Click += new System.EventHandler(this.printScreenButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(177, 57);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(786, 556);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(943, 30);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.searchTextBox.Size = new System.Drawing.Size(170, 20);
+            this.searchTextBox.TabIndex = 8;
+            this.searchTextBox.Text = "search";
+            this.searchTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.searchTextBox_KeyDown);
+            // 
+            // pictureBoxOptionsToolStripMenuItem
+            // 
+            this.pictureBoxOptionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem,
+            this.drawToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.printToolStripMenuItem});
+            this.pictureBoxOptionsToolStripMenuItem.Name = "pictureBoxOptionsToolStripMenuItem";
+            this.pictureBoxOptionsToolStripMenuItem.Size = new System.Drawing.Size(117, 20);
+            this.pictureBoxOptionsToolStripMenuItem.Text = "PictureBoxOptions";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Hide";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // drawToolStripMenuItem
+            // 
+            this.drawToolStripMenuItem.Name = "drawToolStripMenuItem";
+            this.drawToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.drawToolStripMenuItem.Text = "Draw";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Show";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 665);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.printScreenButton);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.homeButton);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Kopeftache";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        
+       
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem navigateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem forwardToolStripMenuItem;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button homeButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button printScreenButton;
+        private PictureBox pictureBox2;
+        private TextBox searchTextBox;
+        private ToolStripMenuItem pictureBoxOptionsToolStripMenuItem;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private ToolStripMenuItem drawToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem printToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+    }
+}
+
---
+++ OsProject/Form1.cs
@@ -0,0 +1,260 @@
+﻿using ScreenShotDemo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OsProject
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(new Uri(comboBox1.SelectedItem.ToString()));
+        }
+
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void backToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void forwardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            webBrowser1.GoHome();
+            MessageBox.Show("                                       Welcome to Kopeftache! \n\n     With this browser, surfing the internet has just become easier!");
+            webBrowser1.ProgressChanged += new WebBrowserProgressChangedEventHandler(delegate (object sender1, WebBrowserProgressChangedEventArgs events)
+            {
+                if (((int)events.CurrentProgress > 0) && ((int)events.MaximumProgress >0))
+                {
+                    toolStripProgressBar1.Maximum = Convert.ToInt32((int)events.MaximumProgress);
+                    toolStripProgressBar1.Step = Convert.ToInt32((int)events.CurrentProgress);
+                    toolStripProgressBar1.PerformStep();
+                } else
+                {
+                    toolStripProgressBar1.Value = (0);
+                }
+            });
+        }
+
+        private void comboBox1_MouseHover(object sender, EventArgs e)
+        {
+            //comboBox1
+        }
+
+        private void toolStripProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public Bitmap GenerateScreenshot(string url)
+        {
+            // This method gets a screenshot of the webpage
+            // rendered at its full size (height and width)
+            return GenerateScreenshot(url, -1, -1);
+        }
+
+        public Bitmap GenerateScreenshot(string url, int width, int height)
+        {
+            // Load the webpage into a WebBrowser control
+            WebBrowser wb = new WebBrowser();
+            wb.ScrollBarsEnabled = false;
+            wb.ScriptErrorsSuppressed = true;
+            wb.Navigate(url);
+            while (wb.ReadyState != WebBrowserReadyState.Complete) { Application.DoEvents(); }
+
+
+            // Set the size of the WebBrowser control
+            wb.Width = width;
+            wb.Height = height;
+
+            if (width == -1)
+            {
+                // Take Screenshot of the web pages full width
+                wb.Width = wb.Document.Body.ScrollRectangle.Width;
+            }
+
+            if (height == -1)
+            {
+                // Take Screenshot of the web pages full height
+                wb.Height = wb.Document.Body.ScrollRectangle.Height;
+            }
+
+            // Get a Bitmap representation of the webpage as it's rendered in the WebBrowser control
+            Bitmap bitmap = new Bitmap(wb.Width, wb.Height);
+            wb.DrawToBitmap(bitmap, new Rectangle(0, 0, wb.Width, wb.Height));
+            wb.Dispose();
+
+            return bitmap;
+        }
+        //private Bitmap MyImage;
+        private void printScreenButton_Click(object sender, EventArgs e)
+        {
+            //method 1
+            //generate a screenshot
+            //Bitmap pic= GenerateScreenshot("http://google.ro");
+            //show it in a picturebox
+            //pictureBox2.Image = pic;
+            //pictureBox2.Visible = true;
+
+            //method 2
+            //ScreenCapture sc = new ScreenCapture();
+            //// capture entire screen, and save it to a file
+            //Image img = sc.CaptureScreen();
+            //// display image in a Picture control named imageDisplay
+            //pictureBox2.Image = img;
+            //pictureBox2.Visible = true;
+            //// capture this window, and save it
+            //sc.CaptureWindowToFile(Handle, "C://temp2.gif", ImageFormat.Gif);
+
+            //method 3
+            var image = ScreenCapture.CaptureActiveWindow();
+            pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox2.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox2.Image = image;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Visible = true;
+            //try
+            //{
+            //    image.Save(@"C:\\temp\\snippetsource.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            //}
+            //catch(Exception ex)
+            //{
+            //    //MessageBox.Show(ex.ToString());
+            //}
+
+            //method 4
+            //Rectangle bounds = this.Bounds;
+            //using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            //{
+            //    using (Graphics g = Graphics.FromImage(bitmap))
+            //    {
+            //        g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+            //    }
+            //    bitmap.Save("C://test.jpg", ImageFormat.Jpeg);
+            //}
+
+            //method 5
+            /*
+            // Sets up an image object to be displayed.
+            if (MyImage != null)
+            {
+                MyImage.Dispose();
+            }
+
+            // Stretches the image to fit the pictureBox.
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            MyImage = pic;
+            pictureBox2.Visible = true;
+            pictureBox2.BackgroundImage = MyImage;
+            */
+        }
+
+        //event handler for the google search engine
+        private void searchTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                webBrowser1.Navigate("http://" +"google.com/search?q=" + searchTextBox.Text);
+            }
+        }
+
+        //hide pictureBox button
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+        }
+
+        //show pictureBox button
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+        }
+        
+        //method for saving the image
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Image image = pictureBox2.Image;
+            try
+            {
+                image.Save(@"C:\\temp\\snippetsource.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+            }
+        }
+
+        //print and save the document as a PDF
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PrintDocument printDocument1 = new PrintDocument();
+            printDocument1.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            printDocument1.Print();
+        }
+        //event handler for the print option
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(pictureBox2.Image, 0, 0);
+        }
+
+        //drawing in the picture box
+        bool draw = false;
+        int s = 3;
+        Color color = Color.Red;
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            draw = true;
+            Graphics g = Graphics.FromImage(pictureBox2.Image);
+            Pen pen1 = new Pen(color, 4);
+            g.DrawRectangle(pen1, e.X, e.Y, 2, 2);
+            g.Save();
+            pictureBox2.Image = pictureBox2.Image;
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draw)
+            {
+                Graphics g = Graphics.FromImage(pictureBox2.Image);
+                SolidBrush brush = new SolidBrush(color);
+                g.FillRectangle(brush, e.X, e.Y, s, s);
+                g.Save();
+                pictureBox2.Image = pictureBox2.Image;
+            }
+        }
+    }
+}
